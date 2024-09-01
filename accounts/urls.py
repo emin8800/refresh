924ad9be4bc67999a090f618d6c3a4b1f77fb368
emin8800 <hesenovemin58@gmail.com>
2024-09-01 22:16:29 +0400

    first commit

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import register_user, user_login, user_logout, csrf_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
+urlpatterns = [
+    path('api/register/', register_user),
+    path('api/login/', user_login),
+    path('api/logout/', user_logout),
+    path('api/csrf-token/', csrf_token),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+]
