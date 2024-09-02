8efb5f9a2ef53316cfbde02cf20a2be90b986b80
Emin <99510258+emin8800@users.noreply.github.com>
2024-09-02 15:20:15 +0400

    Update settings.py

--- drf/settings.py
+++ drf/settings.py
@@ -158,7 +158,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': True,
+    'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,  # Token'ları blacklist'e ekler
     'UPDATE_LAST_LOGIN': False,
     'ALGORITHM': 'HS256',
