7fd6739e20a0bfa12ea5536dcd4b95360a896132
Emin <99510258+emin8800@users.noreply.github.com>
2024-09-02 15:55:29 +0400

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,78 +1,14 @@
-# from rest_framework import status
-# from rest_framework.response import Response
-# from rest_framework.decorators import api_view,permission_classes
-# from .serializers import UserSerializer
-# from rest_framework.authtoken.models import Token
-# from django.contrib.auth import authenticate
-# from django.core.exceptions import ObjectDoesNotExist
-# from .models import CustomUser
-# from rest_framework.permissions import IsAuthenticated
-# from django.http import JsonResponse
-# from django.middleware.csrf import get_token
-
-# @api_view(['POST'])
-# def register_user(request):
-#     if request.method == 'POST':
-#         serializer = UserSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-
-# @api_view(['POST'])
-# def user_login(request):
-#     if request.method == 'POST':
-#         username = request.data.get('username')
-#         password = request.data.get('password')
-
-#         user = None
-#         if '@' in username:
-#             try:
-#                 user = CustomUser.objects.get(email=username)
-#             except ObjectDoesNotExist:
-#                 pass
-
-#         if not user:
-#             user = authenticate(username=username, password=password)
-
-#         if user:
-#             token, _ = Token.objects.get_or_create(user=user)
-#             return Response({'token': token.key}, status=status.HTTP_200_OK)
-
-#         return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-    
-
-
-
-# @api_view(['POST'])
-# @permission_classes([IsAuthenticated])
-# def user_logout(request):
-#     try:
-#         # Kullanıcının auth token'ını sil
-#         request.user.auth_token.delete()
-#         return Response({'message': 'Başarıyla çıkış yapıldı.'}, status=status.HTTP_200_OK)
-#     except Exception as e:
-#         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-
-
-# def csrf_token(request):
-#     return JsonResponse({'csrfToken': get_token(request)})
-
-
-
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
-from .serializers import UserSerializer
-from .models import CustomUser
+from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth import authenticate
+from rest_framework.views import APIView  # Burada APIView'ı import edin
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.http import JsonResponse
 from django.middleware.csrf import get_token
+from django.contrib.auth import authenticate
+from .serializers import UserSerializer
+from .models import CustomUser
 
 @api_view(['POST'])
 def register_user(request):
@@ -98,12 +34,6 @@ def user_login(request):
 
     return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.permissions import IsAuthenticated
-
 class LogoutView(APIView):
     permission_classes = (IsAuthenticated,)
 
@@ -115,11 +45,7 @@ class LogoutView(APIView):
 
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
-
-
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 def csrf_token(request):
     return JsonResponse({'csrfToken': get_token(request)})
-
