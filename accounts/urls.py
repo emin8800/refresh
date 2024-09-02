e1f5adceda5c8b14f2fdef0a38799950e487e70f
Emin <99510258+emin8800@users.noreply.github.com>
2024-09-02 15:55:46 +0400

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import register_user, user_login, user_logout, csrf_token
+from .views import register_user, user_login, LogoutView, csrf_token
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('api/register/', register_user),
     path('api/login/', user_login),
-    path('api/logout/', user_logout),
+    path('api/logout/', LogoutView.as_view(), name='logout'),
     path('api/csrf-token/', csrf_token),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
