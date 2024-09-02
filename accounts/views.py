4d68f01870f6a070a51551b43bbc20401e673cc5
Emin <99510258+emin8800@users.noreply.github.com>
2024-09-02 16:17:28 +0400

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -34,18 +34,26 @@ def user_login(request):
 
     return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
 class LogoutView(APIView):
     permission_classes = (IsAuthenticated,)
 
     def post(self, request):
         try:
-            refresh_token = request.data["refresh"]
-            token = RefreshToken(refresh_token)
-            token.blacklist()  # Token'ı kara listeye ekler
+            refresh_token = request.data.get('refresh')
+            if refresh_token:
+                token = RefreshToken(refresh_token)
+                token.blacklist()  # Token'ı kara listeye ekler
 
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
 
 def csrf_token(request):
     return JsonResponse({'csrfToken': get_token(request)})
